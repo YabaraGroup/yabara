1fa0a9d29fd1d276a3b533e29f2f9484e09ed205
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-09-03 18:13:56 +0200

    ✨ (EmailInput.tsx): Improve styling and responsiveness of EmailInput component 📝 (index.css): Update color variables and add responsive styles for background 📝 (Soon.tsx): Enhance layout and content of Soon page for better user experience

--- client/src/components/EmailInput.tsx
+++ client/src/components/EmailInput.tsx
@@ -2,22 +2,27 @@ import { ArrowRight } from 'lucide-react';
 
 export default function EmailInput() {
   return (
-    <div className="flex items-center bg-[#49709A] rounded-full overflow-hidden w-full max-w-md my-4 text-xl">
+    <div className="flex flex-col sm:flex-row sm:items-center sm:bg-[#49709A] sm:rounded-full sm:overflow-hidden w-full max-w-md my-4 text-xl gap-2 sm:gap-0">
+      {/* Input */}
       <input
         type="email"
         placeholder="Adresse e-mail"
-        className="flex-1 px-6 py-4 bg-transparent text-white placeholder-white focus:outline-none"
+        className="px-6 py-4 rounded-full sm:rounded-none sm:flex-1 
+                   bg-[#49709A] sm:bg-transparent 
+                   text-white placeholder-white focus:outline-none"
       />
 
+      {/* Bouton */}
       <button
-        className="group relative flex items-center gap-3 bg-gold hover:bg-dark-gold 
-                   text-gray-900 font-semibold px-5 py-4 rounded-full transition-colors 
-                   focus:outline-none focus:ring-2 focus:ring-white/50"
+        className="group relative flex items-center justify-between	gap-3 
+                   bg-gold hover:bg-dark-gold text-gray-900 
+                   px-5 py-4 rounded-full sm:rounded-full 
+                   transition-colors focus:outline-none focus:ring-2 focus:ring-white font-light"
       >
         <span>Me notifier</span>
 
-        <span className="grid place-items-center w-8 h-8 rounded-full bg-white/60 overflow-hidden">
-          <ArrowRight className="w-5 h-5 transition-transform duration-300 group-hover:translate-x-1" />
+        <span className="grid place-items-center w-8 h-8 rounded-full bg-white/60 overflow-hidden transition-transform duration-300 group-hover:translate-x-2">
+          <ArrowRight className="w-5 h-5" />
         </span>
       </button>
     </div>
