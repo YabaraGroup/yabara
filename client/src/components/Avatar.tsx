69da58100a5177c56d947af793485d9a23ecce44
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-09-22 15:53:19 +0200

    ✨ (client): add multiple avatar images for men and women to be used in the application 📝 (client/components/Avatar.tsx): update Avatar component to use specific images based on the id provided

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -1,8 +1,12 @@
+import image_1 from '../assets/comingsoon/man1.png';
+import image_2 from '../assets/comingsoon/woman1.png';
+import image_3 from '../assets/comingsoon/man2.png';
+
 function Avatar({ id }: { id: number }) {
   return (
     <figure className="w-24 h-24 bg-white rounded-full mb-4 flex items-center justify-center">
       <img
-        src={`https://avatar.iran.liara.run/public/${id}`}
+        src={id === 1 ? image_1 : id === 2 ? image_2 : image_3}
         alt="Avatar"
         className="rounded-full w-[90px] h-[90px]"
       />
