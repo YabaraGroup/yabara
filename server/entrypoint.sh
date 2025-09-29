ad3199b6239ac3c55e637c8397994ef4229ff9cd
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-09-29 19:12:56 +0200

    🚀 (entrypoint.sh): add support for waiting for MySQL database connection before running migrations ✨ (package.json): introduce separate scripts for running migrations in production and development environments ♻️ (tsconfig.json): update include paths to include src and bin directories for better TypeScript compilation

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 set -e
 
+echo "⏳ Waiting for MySQL..."
+until nc -z -v -w30 "$DB_HOST" "$DB_PORT"
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
 echo "🚀 Running migrations..."
-npm run db:migrate --workspace=server
+if [ "$NODE_ENV" = "production" ]; then
+  npm run db:migrate:prod --workspace=server
+else
+  npm run db:migrate:dev --workspace=server
+fi
 
 echo "✅ Starting server..."
 exec node dist/server.js
